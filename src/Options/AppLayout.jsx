7249f98a93ff22e2604072d5f363cf4009b270a0
developer <team-developer@gmail.com>
2021-05-24 17:52:23 +0800

    upd: theme style customize

--- src/Options/AppLayout.jsx
+++ src/Options/AppLayout.jsx
@@ -1,7 +1,12 @@
 import React, { PureComponent } from 'react';
 
-import { Layout, Button } from 'antd';
+import { Layout, Button, Icon, Image } from 'antd';
+import { MenuFoldOutlined, SettingFilled } from '@ant-design/icons';
+
+import BasLogo from 'UI/widgets/logo';
 // import PropTypes from 'prop-types';
+
+import BasSrc from '../assets/images/bas_big.png';
 
 import ErrorPage from 'Pages/error';
 
@@ -15,19 +20,29 @@ class PageLayout extends PureComponent {
   };
 
   render() {
+    const headStyle = {
+      padding: '0 12px',
+    };
+
     return (
       <Layout>
         <Sider trigger={null} collapsible theme="light">
           <div>Meus</div>
         </Sider>
         <Layout>
-          <Header theme="light">
-            <div>App Header</div>
+          <Header theme="light" style={headStyle}>
+            <div>
+              <Image src={BasSrc} width={40} preview={false} />
+              <SettingFilled style={{ fontSize: '24px' }} />
+            </div>
           </Header>
 
           <Content>
             <ErrorPage error={this.error} />
             <Button type="danger">YOK</Button>
+            <Icon component={<SettingFilled width="100px" />} />
+            <Button type="primary" icon={<SettingFilled />} />
+            <BasLogo style={{ fontSize: '2.75rem' }} />
           </Content>
           <Footer>Foot</Footer>
         </Layout>
